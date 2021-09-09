6ed0d8efc69469a2e6bd4b24b17d606748560a95
shiido <jhonfr29@gmail.com>
2021-09-09 13:18:01 -0500

    feat: Improving command start

--- file-explorer.js
+++ file-explorer.js
@@ -1,19 +1,30 @@
-var cmd = require('node-cmd');
+const chalk = require('chalk');
+const cmd = require('node-cmd');
+
+const log = console.log;
+const warning = chalk.hex('#FFA500');
 
 const args = process.argv.slice(2);
 
-console.log('starting backend ...');
-cmd.run(
-   `cd file-explorer-backend && npm start`, function (err, data, stderr) {
-      console.log('backend ...', err, data, stderr);
-   }
-);
+if (args.length === 0) {
+   log(`\n${chalk.yellowBright('You must specify at least one directory to monitor it')}
+      For example: ${chalk.greenBright('node file-explorer.js ~/Desktop')} \n`);
+   process.exit();
+}
+
+const baseURLBackend = 'http://localhost:3011';
+const baseURLFrontend = 'http://localhost:3000';
+
+log(`\nFile explorer will deploy two services:
+
+   ${chalk.gray('Backend [Nest App]  ')} ---> ${chalk.green(baseURLBackend)}
+   ${chalk.gray('Frontend[React App] ')} ---> ${chalk.greenBright(baseURLFrontend)}
+`);
+
+console.log(warning('Deploying backend service ...'));
+cmd.run('cd file-explorer-backend && npm start');
 
 setTimeout(function () {
-   console.log('starting frontend ...');
-   cmd.run(
-      `cd file-explorer-frontend && REACT_APP_FOLDERS=${args.join(",")} npm start`, function (err, data, stderr) {
-         console.log('frontend ...', err, data, stderr);
-      }
-   );
+   console.log(warning('Deploying frontend service ... \n'));
+   cmd.run(`cd file-explorer-frontend && REACT_APP_FOLDERS=${args.join(",")} npm start`);
 }, 5000);
